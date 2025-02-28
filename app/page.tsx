ff1b6553c687beb0b14360dd886e871e188f7f9e
samuelnihoul <samuelnihoul@tutanota.com>
2025-02-28 20:53:57 +0100

    cam feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // app/page.tsx
 import React from 'react';
 import UserLocation from './components/UserLocation';
+import CameraFeed from './components/CamFeed';
 
 const HomePage: React.FC = () => {
     return (
         <div>
             <h1>Bienvenue à la chasse au trésor !</h1>
             <UserLocation />
+            <CameraFeed />
         </div>
     );
 };
