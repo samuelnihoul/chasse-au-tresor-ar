5ab9e1971c00f0a360f2ec488b5c2e1b7dd0134f
samuelnihoul <samuelnihoul@tutanota.com>
2025-03-07 19:13:57 +0100

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <nav className="p-4 border-b ">
+        <Link href="/admin">Admin</Link>
+      </nav>
         {children}
       </body>
     </html>
