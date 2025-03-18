01c484587a7b3e575e395c412fea4f2535a064d5
samuelnihoul <samuelnihoul@tutanota.com>
2025-03-18 12:48:24 +0100

    Revert "todo.md"
    This reverts commit 75112ec2fccd9a2ecdc0276192c4b33ecad53f24.

---
+++ app/components/CamFeed.tsx
@@ -0,0 +1,43 @@
+// app/components/CameraFeed.tsx
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+
+const CameraFeed: React.FC = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        const startVideo = async () => {
+            if (videoRef.current) {
+                try {
+                    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                    videoRef.current.srcObject = stream;
+                } catch (error) {
+                    console.error("Erreur d'accès à la caméra :", error);
+                }
+            }
+        };
+
+        startVideo();
+
+        return () => {
+            // Arrêter le flux vidéo lors de la désinstallation du composant
+            if (videoRef.current && videoRef.current.srcObject) {
+                const stream = videoRef.current.srcObject as MediaStream;
+                const tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+            }
+        };
+    }, []);
+
+    return (
+        <video
+            ref={videoRef}
+            autoPlay
+            playsInline
+            style={{ width: '50%', height: 'auto', marginTop: '20px' ,display:'flex',justifyContent:'center',alignItems:'center',border:'1px solid #ccc'}}
+        />
+    );
+};
+
+export default CameraFeed;
